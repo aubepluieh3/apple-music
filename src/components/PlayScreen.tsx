1eb942db0271f87b015a20149039b8e8b6659c7e
soo <aubepluieh3@naver.com>
2022-11-01 17:25:21 +0900

    Lyrics

--- src/components/PlayScreen.tsx
+++ src/components/PlayScreen.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import {
   faRepeat,
@@ -13,7 +13,7 @@ import {
   faListUl,
   faVolumeOff,
   faVolumeHigh,
-  faVolumeXmark
+  faVolumeXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { faMessage } from "@fortawesome/free-regular-svg-icons";
 import React from "react";
@@ -111,12 +111,48 @@ const PlayBox = styled.div`
 const Another = styled.div`
   display: flex;
   justify-content: space-between;
-  margin: 30px 40px;
+  svg {
+    margin: 30px 40px;
+  }
 `;
+
+//
+
 const Btn = styled.button`
   background: none;
 
   border: none;
+`;
+
+const BoxAnimation = styled(motion.div)`
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  width: 400px;
+  height: 300px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  position: absoulte;
+  overflow-x: scroll;
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  bottom: 520px;
+  right: 105px;
+`;
+
+const overlay = {
+  hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  visible: { backgroundColor: "rgba(0,0,0,0)" },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+};
+const Lyrics = styled.div`
+  font-size: 15px;
+  padding: 25px;
+  text-align: center;
 `;
 
 function PlayScreen({ tracks }) {
@@ -127,8 +163,9 @@ function PlayScreen({ tracks }) {
   const [trackIndex, setTrackIndex] = useState(0);
   const [barLength, setBarLength] = useState(0);
   const [trackProgress, setTrackProgress] = useState(0);
+  const [id, setId] = useState<null | string>(null);
 
-  const { title, artist, id, music, lyrics, img } = tracks[trackIndex];
+  const { title, artist, music, lyrics, img } = tracks[trackIndex];
 
   // Refs
   const audioRef = useRef(new Audio(music));
@@ -251,6 +288,7 @@ function PlayScreen({ tracks }) {
 
   //shuffle
   const onShuffle = () => {
+    clearInterval(intervalRef.current);
     setTrackIndex(Math.floor(Math.random() * tracks.length));
   };
 
@@ -327,10 +365,44 @@ function PlayScreen({ tracks }) {
         />
         <FontAwesomeIcon icon={faVolumeHigh} />
       </SoundBar>
+
       <Another>
-        <Btn>
-          <FontAwesomeIcon icon={faMessage} />
-        </Btn>
+        {tracks
+          .filter((track: { id: string }) => track.id === String(trackIndex))
+          .map((track) => (
+            <div
+              layoutId={track.id}
+              key={track.id}
+              onClick={() => setId(track.id)}
+            >
+              {" "}
+              <Btn>
+                <FontAwesomeIcon icon={faMessage} />
+              </Btn>
+            </div>
+          ))}
+        <AnimatePresence>
+          {id ? (
+            <Overlay
+              variants={overlay}
+              onClick={() => setId(null)}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            >
+              <BoxAnimation layoutId={id}>
+                <Lyrics>
+                  {lyrics.map((lyric) => (
+                    <>
+                      <span>{lyric}</span>
+                      <br />
+                    </>
+                  ))}
+                </Lyrics>
+              </BoxAnimation>
+            </Overlay>
+          ) : null}
+        </AnimatePresence>
         <FontAwesomeIcon icon={faListUl} />
       </Another>
     </Box>
